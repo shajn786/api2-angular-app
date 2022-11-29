9b26ee9ce48fca6d2ccb1f5a43e43f4ba5c9a181
shajn786 <shajn786@gmail.com>
2022-11-29 10:59:47 +0530

    uspublic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PassengerviewallComponent } from './passengerviewall/passengerviewall.component';
+import { UspublicComponent } from './uspublic/uspublic.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PassengerviewallComponent
+    PassengerviewallComponent,
+    UspublicComponent
   ],
   imports: [
     BrowserModule,
