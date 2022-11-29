b82c02244346cfd007d1be3b809bdce06fa67318
shajn786 <shajn786@gmail.com>
2022-11-29 12:12:00 +0530

    til nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,62 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PassengerviewallComponent } from './passengerviewall/passengerviewall.component';
 import { UspublicComponent } from './uspublic/uspublic.component';
+import { PublicApiComponent } from './public-api/public-api.component';
+import { UserinfoComponent } from './userinfo/userinfo.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { UserdataComponent } from './userdata/userdata.component';
+import { TodoComponent } from './todo/todo.component';
+import { QouteComponent } from './qoute/qoute.component';
+import { GetCoursesComponent } from './get-courses/get-courses.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const myRoutes : Routes =[
+  {
+    path:"",component:GetCoursesComponent
+  },
+  {
+    path:"passenger",component:PassengerviewallComponent
+  },
+  {
+    path:"product",component:ProductListComponent
+  },
+  {
+    path:"api",component:PublicApiComponent
+  },
+  {
+    path:"qoutes",component:QouteComponent
+  },
+  {
+    path:"todo",component:TodoComponent
+  },
+  {
+    path:"userdata",component:UserdataComponent
+  },
+  {
+    path:"userinfo",component:UserinfoComponent
+  }
+]
+
 
 @NgModule({
   declarations: [
     AppComponent,
     PassengerviewallComponent,
-    UspublicComponent
+    UspublicComponent,
+    PublicApiComponent,
+    UserinfoComponent,
+    ProductListComponent,
+    UserdataComponent,
+    TodoComponent,
+    QouteComponent,
+    GetCoursesComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
